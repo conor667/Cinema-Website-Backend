d30f3ac841eacc451ba3d666a5f1107abb553cca
Robert-Kusz <kooshservers@googlemail.com>
2022-09-07 15:30:12 +0100

    stripe api payment setup

--- index.js
+++ index.js
@@ -4,6 +4,8 @@ const database = require('./config/database.config');
 const authenticationRouter = require('./route/AuthenticationRouter');
 const exampleRouter = require('./route/ExampleRouter');
 const cors = require('cors');
+const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY);
+
 
 const PORT = process.env.PORT || 5500;
 
@@ -16,7 +18,7 @@ app.use(express.static("public"));
 
 app.use(authenticationRouter);
 app.use('/example', exampleRouter);
-
+app.use("/api/stripe",stripe);
 async function main() {
     await database.connect();
 
