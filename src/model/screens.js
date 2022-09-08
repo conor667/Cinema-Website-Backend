cc76e00214a2bef6acccd2741d3de5e27f338b5b
Conor White <conorwhite667@gmail.com>
2022-09-08 09:30:56 +0100

    added screen schema

---
+++ src/model/screens.js
@@ -0,0 +1,30 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+const bcrypt = require('bcrypt');
+
+
+const screenSchema = new Schema({
+    screenName: {
+        type: String,
+        default: null,
+        required: [true, 'Please enter a screen name'],
+        trim: true,
+        unique: true
+    },
+    screenNumber:{
+        type: Number,
+        required: [true, 'Please enter the screen number']
+    },
+    seats:{
+        type: Number,
+        required: [true, 'Please enter the amount of seats the screen has']
+    },
+    film:{
+        _id: String,
+        name: String,
+    }
+});
+
+const screen = mongoose.model('Screen', screenSchema);
+
+module.exports = User;
