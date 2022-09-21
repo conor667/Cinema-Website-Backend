c2f1968b2e28e597ef3441a4bea89a122bce90c7
JustaGlitch <justingraham1108@gmail.com>
2022-09-21 17:25:13 +0100

    Adding movie to database test

--- index.js
+++ index.js
@@ -1,47 +1,51 @@
-const express = require('express');
-const cors = require('cors');
+const express = require("express");
+const cors = require("cors");
 const mongoose = require("mongoose");
+const cookieParser = require("cookie-parser");
 
 //Routers
-const userRouter = require('./src/router/userRouter');
-const movieRouter = require('./src/router/movieRouter');
-const screenRouter = require('./src/router/screenRouter')
-const ticketRouter = require('./src/router/TicketRouter');
-const commentRouter = require('./src/router/CommentRouter');
-const authenticationRouter = require('./src/router/AuthenticationRouter');
+const userRouter = require("./src/router/userRouter");
+const movieRouter = require("./src/router/movieRouter");
+const screenRouter = require("./src/router/screenRouter");
+const ticketRouter = require("./src/router/TicketRouter");
+const commentRouter = require("./src/router/CommentRouter");
+const authenticationRouter = require("./src/router/AuthenticationRouter");
 
 //Logger
-const { simpleLogger, logHitSpecialEndpoint } = require('./src/loggers/generic');
+const { simpleLogger, logHitSpecialEndpoint } = require("./src/loggers/generic");
 
 //Database setup
 const PORT = process.env.PORT || 5500;
-const DB_URL = process.env.DB_URL || "mongodb://127.0.0.1:27017/QaCinema"
+const DB_URL = process.env.DB_URL || "mongodb://127.0.0.1:27017/QaCinema";
 const app = express();
 
 //Middleware setup
 app.use(simpleLogger);
 app.use(cors("*"));
 app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
+// app.use(express.urlencoded({ extended: true }));
 
 // setup serving static files
 app.use(express.static("public"));
 
 //Authenticator
 app.use(authenticationRouter);
-app.use('/', userRouter);
-app.use('/', movieRouter);
-app.use('/', screenRouter);
-app.use('/', ticketRouter);
-app.use('/', commentRouter);
+app.use("/", userRouter);
+app.use("/", movieRouter);
+app.use("/", screenRouter);
+app.use("/", ticketRouter);
+app.use("/", commentRouter);
 
 //Main Method
 let server;
-mongoose.connect(DB_URL, { useNewUrlParser: true })
-        .then(() => {
-            console.log(`Database has been connected on mongodb 127.0.0.1:27017`);
-            server = app.listen(PORT, () => console.log(`Server is up on port localhost:${PORT}!`));
-        }).catch(error => {
-            console.log(`Unable to connect to database.`)
-        });
+mongoose
+	.connect(DB_URL, { useNewUrlParser: true })
+	.then(() => {
+		console.log(`Database has been connected on mongodb 127.0.0.1:27017`);
+		server = app.listen(PORT, () => console.log(`Server is up on port localhost:${PORT}!`));
+	})
+	.catch((error) => {
+		console.log(`Unable to connect to database.`);
+	});
 let databaseConnection = mongoose.connection;
--- src/router/movieRouter.js
+++ src/router/movieRouter.js
@@ -1,70 +1,76 @@
-const express = require('express');
-const Movie = require('../model/movie');
-const MovieNotFoundError = require('../errors/MovieNotFoundError');
+const express = require("express");
+const Movie = require("../model/movie");
+const MovieNotFoundError = require("../errors/MovieNotFoundError");
 
 //setting up router
 const router = express.Router();
 
+const seed = new Movie({
+	filmName: "X",
+	imageURL: "https://m.media-amazon.com/images/M/MV5BMTJiMmE5YWItOWZjYS00YTg0LWE0MTYtMzg2ZTY4YjNkNDEzXkEyXkFqcGdeQXVyMTAzMDg4NzU0._V1_.jpg",
+	length: "1h 45m",
+	director: "Ti West",
+	genre: "Horror",
+	description: "In 1979, a group of young filmmakers set out to make an adult film in rural Texas, but when their reclusive, elderly hosts catch them in the act, the cast find themselves fighting for their lives.",
+	rating: "R18",
+});
+seed.save();
+
 //Checks if the body contains Json data
 function isJsonData(request, response, next) {
-    if (request.headers['content-type'] !== 'application/json') {
-        return next(new Error("Route only accepts JSON data."));
-    }
-    next();
+	if (request.headers["content-type"] !== "application/json") {
+		return next(new Error("Route only accepts JSON data."));
+	}
+	next();
 }
-//finds all movies 
+//finds all movies
 router.get("/movie", async (request, response) => {
-    if (request.query.filmName) {
-    
-    }
-    else response.json(await Movie.find()); 
+	if (request.query.filmName) {
+	} else response.json(await Movie.find());
 });
 
 //Find movie by ID
 router.get("/movie/:id", async (request, response, next) => {
-    const movie = await Movie.findById(request.params.id);
+	const movie = await Movie.findById(request.params.id);
 
-    if (movie) {
-        response.status(200).json(movie);
-    } else {
-
-        next(new MovieNotFoundError(request.params.id));
-    }
+	if (movie) {
+		response.status(200).json(movie);
+	} else {
+		next(new MovieNotFoundError(request.params.id));
+	}
 });
 //create a movie
 router.post("/movie", isJsonData, async (request, response, next) => {
-    try {
-        const movie = new Movie(request.body);
-        await movie.save();
+	try {
+		const movie = new Movie(request.body);
+		await movie.save();
 
-        response.status(201)
-                .setHeader("Content-Location", `/movie/${movie.id}`)
-                .json(movie);
-    } catch (error) {
-        next(error);
-    }
+		response.status(201).setHeader("Content-Location", `/movie/${movie.id}`).json(movie);
+	} catch (error) {
+		next(error);
+	}
 });
 
 // update a movie by ID
 router.put("/movie/:id", isJsonData, async (request, response, next) => {
-    const movie = await Movie.updateOne({ _id: request.params.id }, request.body);
+	const movie = await Movie.updateOne({ _id: request.params.id }, request.body);
 
-    if (movie) {
-        response.status(200).json(movie);
-    } else {
-        next(new MovieNotFoundError(request.params.id));
-    }
+	if (movie) {
+		response.status(200).json(movie);
+	} else {
+		next(new MovieNotFoundError(request.params.id));
+	}
 });
 
 // delete a movie by ID
 router.delete("/movie/:id", async (request, response, next) => {
-    const movie = await Movie.findOneAndDelete({ _id: request.params.id });
+	const movie = await Movie.findOneAndDelete({ _id: request.params.id });
 
-    if (movie) {
-        response.status(200).json(movie);
-    } else {
-        next(new MovieNotFoundError(request.params.id));
-    }
+	if (movie) {
+		response.status(200).json(movie);
+	} else {
+		next(new MovieNotFoundError(request.params.id));
+	}
 });
 
 module.exports = router;
